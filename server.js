9c21b45e33938c6820a55b9dd8e7a81692f3eb0e
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-03-09 23:35:00 +0400

    connect to mongodb database

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 import http from "http";
 import dotenv from "dotenv";
 import { handleTasksRoutes } from "./routes/tasks.js";
+import { connectDB } from "./db/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  handleTasksRoutes(req, res);
-});
+async function startServer() {
+  await connectDB();
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  const server = http.createServer((req, res) => {
+    handleTasksRoutes(req, res);
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer().catch((err) => console.error("Failed to start server", err));

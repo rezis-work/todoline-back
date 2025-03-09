8b2bb90b7b05b4608881afc700a9a8e0d7209502
Rezi Karanadze <karanadzerevazi@gmail.com>
2025-03-10 00:23:50 +0400

    implement patch and delete crud operations in task route

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,4 +1,5 @@
 import { getDB } from "../db/db.js";
+import { ObjectId } from "mongodb";
 
 export async function handleTasksRoutes(req, res) {
   const db = await getDB();
@@ -14,7 +15,7 @@ export async function handleTasksRoutes(req, res) {
   } else if (req.method === "POST" && req.url === "/tasks") {
     let body = "";
     req.on("data", (chunck) => {
-      body += chunck;
+      body += chunck.toString();
     });
     req.on("end", async () => {
       const newTask = JSON.parse(body);
@@ -30,6 +31,66 @@ export async function handleTasksRoutes(req, res) {
         })
       );
     });
+  } else if (req.method.startsWith("PATCH") && req.url.startsWith("/tasks/")) {
+    const taskId = req.url.split("/")[2];
+    if (!ObjectId.isValid(taskId)) {
+      res.writeHead(400, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Invalid task ID" }));
+      return;
+    }
+
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", async () => {
+      const updates = JSON.parse(body);
+      const result = await taskCollection.updateOne(
+        { _id: new ObjectId(taskId) },
+        { $set: updates }
+      );
+
+      if (result.matchedCount === 0) {
+        res.writeHead(404, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Task not found" }));
+        return;
+      } else {
+        res.writeHead(200, {
+          "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "Task updated successfully" }));
+      }
+    });
+  } else if (req.method.startsWith("DELETE") && req.url.startsWith("/tasks/")) {
+    const taskId = req.url.split("/tasks/")[1];
+    if (!ObjectId.isValid(taskId)) {
+      res.writeHead(400, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Invalid task ID" }));
+      return;
+    }
+
+    const result = await taskCollection.deleteOne({
+      _id: new ObjectId(taskId),
+    });
+
+    if (result.deletedCount === 0) {
+      res.writeHead(404, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Task not found" }));
+      return;
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "Task deleted successfully" }));
+    }
   } else {
     res.writeHead(404, {
       "Content-Type": "application/json",
